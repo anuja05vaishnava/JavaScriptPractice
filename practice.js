5658f65416d4eefd1bdc69d03e0db67453a94bbf
anuja05vaishnava <60468761+anuja05vaishnava@users.noreply.github.com>
2023-02-09 18:41:24 +0530

    Create practice.js

---
+++ practice.js
@@ -0,0 +1,45 @@
+/*Count Repeated letter*/
+
+let obj={}
+const repeats=[];
+const inputString='javascript'
+
+for( let x = 0, length = str.length; x < length; x++) {
+    var l = str.inputString(x)
+    obj[l] = (isNaN(obj[l]) ? 1 : obj[l] + 1);
+}
+
+console.log(obj)
+
+
+/*Reverse letter*/
+
+constReverseString = inputString.split('').reverse().join('');
+
+/*Check Anagram*/
+
+const str1 = 'Mary';
+const str2 = 'Army'
+constStr1Sorted = str1.split('').sort().join('');
+constStr2Sorted = str1.split('').sort().join('');
+
+const validate = constStr1Sorted === constStr2Sorted ? 'Yes':'No';
+
+/*Check vovels and consonents*/
+
+const InputString = 'Hello World';
+for (let count = 0; count <= userData.legth; count++){
+if((InputString.charAt(count).match(/[aeiou]/))){       
+    a++;
+    e++;
+    i++;
+    o++;
+    u++;
+} else if((InputString.charAt(count).match(/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/))){
+    consonants++;
+}
+}
+
+console.log(consonants)
+
+
